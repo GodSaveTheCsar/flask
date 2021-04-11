51f00767bec3424f3ce5d64ab1213210ac7490f3
GodSaveTheCsar <danil.kozinski@ya.ru>
2021-04-11 09:54:48 +0300

    3 классная

--- data/users.py
+++ data/users.py
@@ -19,15 +19,3 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
-
-session = db_session.create_session()
-user = User()
-user.surname = "Scott"
-user.name = "Ridley"
-user.age = 21
-user.position = "captain"
-user.speciality = "research engineer"
-user.address = "module_1"
-user.email = "scott_chief@mars.org"
-user.hashed_password = "cap"
-session.add(user)
--- main.py
+++ main.py
@@ -9,9 +9,37 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/mars_explorer.db")
     user = User()
-    user.name = "Пользователь 1"
-    user.about = "биография пользователя 1"
-    user.email = "email@email.ru"
+    user.name = "Ridley"
+    user.surname = "Scott"
+    user.email = "scott_chief@mars.org"
+    user.age = 21
+    user.position = 'captain'
+    user.speciality = 'research engineer'
+    user.address = 'module_1'
+
+    user.name = "Ridley"
+    user.surname = "Well"
+    user.email = "scott_engineer@mars.org"
+    user.age = 19
+    user.position = 'yed'
+    user.speciality = 'yull'
+    user.address = 'module_2'
+
+    user.name = "Ridley"
+    user.surname = "Aught"
+    user.email = "scott_bruh@mars.org"
+    user.age = 20
+    user.position = 'rad'
+    user.speciality = 'yell'
+    user.address = 'module_3'
+
+    user.name = "Yalley"
+    user.surname = "Aught"
+    user.email = "scott_yalley@mars.org"
+    user.age = 14
+    user.position = 'ra1d'
+    user.speciality = 'yel1'
+    user.address = 'module_4'
     db_sess = db_session.create_session()
     db_sess.add(user)
     db_sess.commit()
